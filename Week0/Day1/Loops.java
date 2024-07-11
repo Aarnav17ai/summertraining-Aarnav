eee2e5f9d6ca30675dc499b90b5bf4285fd69c4b
aarnav verma <aarnavverma000@gmail.com>
2024-07-11 14:08:34 +0530

    Day1

---
+++ Week0/Day1/Loops.java
@@ -0,0 +1,15 @@
+package Day1;
+
+public class Loops {
+    public static void main(String[] args) {
+        int i= 5;
+        while (i<15){
+            if (i%2==0 && i%7==0) {
+                System.out.println(i);
+                i++;
+            }
+
+            
+        }
+    }
+}
---
+++ Week0/Day1/Operators.java
@@ -0,0 +1,12 @@
+package Day1;
+public class Operators {
+    public static void main(String[] args) {
+        int i;
+        int sum=0;
+        for(i=1;i<=25;i++)
+        {
+        sum=sum+i;
+        System.out.print(sum + "  ");
+        }
+    }
+}
